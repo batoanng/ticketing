42970dd3ef4644e1760fa48825155fb301043732
Toan Nguyen <toan.nguyen@hitatek.io>
2021-03-29 23:20:06 +0700

    add tests for auth module

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,14 +1,24 @@
-import {MongoMemoryServer} from 'mongodb-memory-server';
+import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
-import {app} from '../app';
+import request from 'supertest';
+import { app } from '../app';
+
+declare global {
+    namespace NodeJS {
+        interface Global {
+            signin(): Promise<string[]>;
+        }
+    }
+}
 
 let mongo: any;
 beforeAll(async () => {
-    // create env variables for testing
-    process.env.JWT_SECRET = "test";
-    
+    process.env.JWT_KEY = 'asdfasdf';
+    process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
     mongo = new MongoMemoryServer();
     const mongoUri = await mongo.getUri();
+
     await mongoose.connect(mongoUri, {
         useNewUrlParser: true,
         useUnifiedTopology: true
@@ -17,6 +27,7 @@ beforeAll(async () => {
 
 beforeEach(async () => {
     const collections = await mongoose.connection.db.collections();
+
     for (let collection of collections) {
         await collection.deleteMany({});
     }
@@ -26,3 +37,20 @@ afterAll(async () => {
     await mongo.stop();
     await mongoose.connection.close();
 });
+
+global.signin = async () => {
+    const email = 'test@test.com';
+    const password = 'password';
+
+    const response = await request(app)
+        .post('/api/users/signup')
+        .send({
+            email,
+            password
+        })
+        .expect(201);
+
+    const cookie = response.get('Set-Cookie');
+
+    return cookie;
+};
