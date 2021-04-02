6533c357f3f973d22aab8958e3c9d74990a1aaef
Toan Nguyen <toan.nguyen@hitatek.io>
2021-04-02 23:59:15 +0700

    implement use request hook

---
+++ client/hooks/use-request.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+import { useState } from 'react';
+
+export default ( url, method, body ) => {
+	const [error , setError] = useState(null);
+
+	const doRequest = async () => {
+		try {
+			const res = await axios[method](url, body);
+			setError(null);
+			return res.data;
+		} catch (e) {
+			const errors = e.response && e.response.data && e.response.data.errors;
+			errors && errors.length > 0 && setError(
+				<div className="alert alert-danger">
+					<h4>Oops...</h4>
+					<ul className="my-0">
+						{errors.map(error => <li key={error.message}>{error.message}</li>)}
+					</ul>
+				</div>
+			);
+		}
+	};
+
+	return { doRequest, error };
+}
--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,26 +1,27 @@
 import { useState } from 'react';
-import axios from 'axios';
 import getConfig from "next/config";
+import useRequest from '../../hooks/use-request';
+
 const { publicRuntimeConfig } = getConfig();
 const AUTH_SERVICE = publicRuntimeConfig.REACT_APP_AUTH_SERVICE ? publicRuntimeConfig.REACT_APP_AUTH_SERVICE : '';
 
 const SignUp = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [errors, setErrors] = useState([]);
+    const { doRequest, error } = useRequest(
+        `${AUTH_SERVICE}/api/users/signup`,
+        'post',
+        {
+            email: email,
+            password: password
+        }
+    );
 
     const onSubmit = async (event) => {
         event.preventDefault();
 
-        try {
-            const res = await axios.post(`${AUTH_SERVICE}/api/users/signup`, {
-                email: email,
-                password: password
-            });
-            setErrors([]);
-        } catch (e) {
-            e.response && e.response.data && setErrors(e.response.data.errors);
-        }
+        const data = await doRequest();
+        console.log(data);
     };
 
     return (
@@ -43,12 +44,7 @@ const SignUp = () => {
                     className="form-control"
                 />
             </div>
-            {errors.length > 0 && <div className="alert alert-danger">
-                <h4>Oops...</h4>
-                <ul className="my-0">
-                    {errors.map(error => <li key={error.message}>{error.message}</li>)}
-                </ul>
-            </div>}
+            {error}
             <button className="btn btn-primary">Sign Up</button>
         </form>
     );
