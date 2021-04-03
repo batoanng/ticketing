5ed0aabee011570edccdf000c3d4993bcca13cbf
Toan Nguyen <toan.nguyen@hitatek.io>
2021-04-03 21:27:03 +0700

    remove redundant code

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,35 +1,12 @@
 import buildClient from '../apis/build-client';
-import getConfig from "next/config";
-
-const { publicRuntimeConfig } = getConfig();
-const AUTH_SERVICE = publicRuntimeConfig.REACT_APP_AUTH_SERVICE ? publicRuntimeConfig.REACT_APP_AUTH_SERVICE : '';
 
 const LandingPage = ({ currentUser }) => {
-    console.log(currentUser)
-    return (
-        <div className='container'>
-            haha
-        </div>
-    );
+    return
+        currentUser ? (<h1>You are sign in</h1>): (<h1>You are NOT sign in</h1>)
+    ;
 }
 
 LandingPage.getInitialProps = async context => {
-    // if (typeof window === "undefined") {
-    //     // This is called from server, which is inside a pod
-    //     // Need to call to another namespace
-    //     const { data } = await axios.get(
-    //         `http://ingress-nginx.ingress-nginx.svc.cluster.local/api/users/currentuser`,
-    //         {
-    //             // Define the host to go to correct server and get the cookie
-    //             headers: req.headers
-    //         }
-    //     );
-    //     return data;
-    // } else {
-    //     // This is called from browser
-    //     const { data } = await axios.get(`/api/users/currentuser`);
-    //     return data
-    // }
     const { data } = buildClient(context).get(`/api/users/currentuser`);
     return data;
 }
