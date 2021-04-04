852a878f1344dc287a066f4662db5187f8c5111e
Toan Nguyen <toan.nguyen@hitatek.io>
2021-04-04 23:25:50 +0700

    move out common code to common module

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,7 +1,7 @@
 import express, {Request, Response} from 'express';
 import { body } from 'express-validator';
-import {validateRequest} from '../middlewares/validate-request';
-import { BadRequestError } from '../errors/bad-request-error';
+import {validateRequest} from '../../../common/src/middlewares/validate-request';
+import { BadRequestError } from '../../../common/src/errors/bad-request-error';
 import {User} from '../models/user';
 import { Password } from '../services/password';
 import jwt from 'jsonwebtoken';
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,7 @@
 import express, {Request, Response} from 'express';
 import { body } from 'express-validator';
-import { BadRequestError } from '../errors/bad-request-error';
-import {validateRequest} from '../middlewares/validate-request';
+import { BadRequestError } from '../../../common/src/errors/bad-request-error';
+import {validateRequest} from '../../../common/src/middlewares/validate-request';
 import {User} from '../models/user';
 import jwt from 'jsonwebtoken';
 
