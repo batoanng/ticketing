947847192af4a8e553fa5eb16bf0613ff0ac42a8
Toan Nguyen <toan.nguyen@hitatek.io>
2021-04-06 00:38:10 +0700

    update version

--- common/src/errors/not-authorized-error.ts
+++ common/src/errors/not-authorized-error.ts
@@ -1,7 +1,7 @@
 import { CustomerError } from "./custom-error";
 
 export class NotAuthorizedError extends CustomerError {
-  errorCode = 400;
+  errorCode = 401;
   constructor() {
     super("Not Authorized");
     Object.setPrototypeOf(this, NotAuthorizedError.prototype);
