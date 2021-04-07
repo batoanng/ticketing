6ad49b784ffd0ce7ecfb5981c96e2e1f5611f0b2
joker7t <batoan131196@gmail.com>
2021-04-07 22:06:49 +0700

    fix test in show file

--- tickets/src/routes/__test__/show.test.ts
+++ tickets/src/routes/__test__/show.test.ts
@@ -1,12 +1,14 @@
 import request from "supertest";
 import { app } from "../../app";
+import mongoose from "mongoose";
 
 it("should return not found error for invalid ticket id", async function () {
+  const id = mongoose.Types.ObjectId().toHexString();
   await request(app)
-    .get(`/api/tickets/wrongId`)
+    .get(`/api/tickets/${id}`)
     .set("Cookie", global.signin())
     .send({})
-    .expect(400);
+    .expect(404);
 });
 
 it("should return a ticket for valid ticket id", async function () {
