19cbb6543e9a29e573fc0131d419b1738ed22782
Toan Nguyen <toan.nguyen@hitatek.io>
2021-04-11 17:34:34 +0700

    remove test

--- nats-test/src/events/ticket-created-publisher.ts
+++
@@ -1,7 +0,0 @@
-import { Publisher } from "./base-publisher";
-import { Subjects } from "./subjects";
-import { TicketCreatedEvent } from "./ticket-created-event";
-
-export default class TicketCreatedPublisher extends Publisher<TicketCreatedEvent> {
-  readonly subject = Subjects.TicketCreated;
-}
