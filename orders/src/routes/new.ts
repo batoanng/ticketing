2f06d579a5d754000a1c57f4f2546312d1e09b27
Toan Nguyen <toan.nguyen@hitatek.io>
2021-04-15 20:39:51 +0700

    complete add order

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -13,6 +13,9 @@ import { Order } from "../models/order";
 
 const router = express.Router();
 
+const EXPIRATION_WINDOW_SECONDS =
+  Number.parseInt(process.env.EXPIRATION_WINDOW_SECONDS!) || 15 * 60;
+
 router.post(
   "/api/orders",
   requireAuth,
@@ -25,15 +28,26 @@ router.post(
   ],
   validateRequest,
   async (req: Request, res: Response) => {
+    console.log(1212);
     const { ticketId } = req.body;
     const ticket = await Ticket.findById(ticketId);
     if (!ticket) {
       throw new NotFoundError();
     }
-    if (ticket.isReserved()) {
+    const isReserved = await ticket.isReserved();
+    if (isReserved) {
       throw new BadRequestError("Ticket is already reserved");
     }
-    return res.send({});
+    const expiration = new Date();
+    expiration.setSeconds(expiration.getSeconds() + EXPIRATION_WINDOW_SECONDS);
+    const order = Order.build({
+      userId: req.currentUser!.id,
+      status: OrderStatus.CREATED,
+      expireAt: expiration,
+      ticket,
+    });
+    await order.save();
+    return res.status(201).send({ order });
   }
 );
 
