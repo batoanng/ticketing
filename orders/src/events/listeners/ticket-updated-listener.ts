90d23add0250c8df2e3f23ef4306ad62073ee439
joker7t <batoan131196@gmail.com>
2021-04-22 23:53:53 +0700

    add ticket updated listener

--- orders/src/events/listeners/ticket-updated-listener.ts
+++ orders/src/events/listeners/ticket-updated-listener.ts
@@ -3,15 +3,25 @@ import {
   Subjects,
   TicketCreatedEvent,
   Listener,
+  NotFoundError,
 } from "@joker7nbt-ticketing/common";
 import { Message } from "node-nats-streaming";
 import { QUEUE_GROUP_NAME } from "../queue-group-name";
+import { Ticket } from "../../models/ticket";
 
 export class TicketUpdatedListener extends Listener<TicketUpdatedEvent> {
   subject: Subjects.TicketUpdated = Subjects.TicketUpdated;
   queueGroupName = QUEUE_GROUP_NAME;
 
-  onMessage(data: TicketCreatedEvent["data"], msg: Message) {
-    console.log(msg);
+  async onMessage(data: TicketCreatedEvent["data"], msg: Message) {
+    const { id, title, price } = data;
+    const ticket = await Ticket.findById(id);
+    if (!ticket) {
+      throw new NotFoundError();
+    }
+    ticket.set({ title, price });
+    await ticket.save();
+
+    msg.ack();
   }
 }
