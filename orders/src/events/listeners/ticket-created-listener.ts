d31288932e34a81626bd444e69e2a2e28dcf51c8
joker7t <batoan131196@gmail.com>
2021-04-22 23:43:10 +0700

    base for ticket create listener

--- orders/src/events/listeners/ticket-created-listener.ts
+++ orders/src/events/listeners/ticket-created-listener.ts
@@ -4,11 +4,23 @@ import {
   Listener,
 } from "@joker7nbt-ticketing/common";
 import { Message } from "node-nats-streaming";
+import { QUEUE_GROUP_NAME } from "../queue-group-name";
+import { Ticket } from "../../models/ticket";
 
 export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
   readonly subject = Subjects.TicketCreated;
-  queueGroupName = "orders-service";
-  onMessage(data: TicketCreatedEvent["data"], msg: Message) {
-    console.log(msg);
+  queueGroupName = QUEUE_GROUP_NAME;
+
+  async onMessage(data: TicketCreatedEvent["data"], msg: Message) {
+    const { title, price } = data;
+
+    const ticket = Ticket.build({
+      title,
+      price,
+    });
+
+    await ticket.save();
+
+    msg.ack();
   }
 }
--- orders/src/events/listeners/ticket-updated-listener.ts
+++ orders/src/events/listeners/ticket-updated-listener.ts
@@ -5,10 +5,12 @@ import {
   Listener,
 } from "@joker7nbt-ticketing/common";
 import { Message } from "node-nats-streaming";
+import { QUEUE_GROUP_NAME } from "../queue-group-name";
 
 export class TicketUpdatedListener extends Listener<TicketUpdatedEvent> {
   subject: Subjects.TicketUpdated = Subjects.TicketUpdated;
-  queueGroupName = "test";
+  queueGroupName = QUEUE_GROUP_NAME;
+
   onMessage(data: TicketCreatedEvent["data"], msg: Message) {
     console.log(msg);
   }
