54fe25aafa3bb18ac841167d5c0afa426e80b370
Toan Nguyen <toan.nguyen@hitatek.io>
2021-04-24 09:46:50 +0700

    add event for order action in ticket module

---
+++ tickets/src/events/listeners/order-cancelled-listener.ts
@@ -0,0 +1,17 @@
+import {
+  Subjects,
+  Listener,
+  OrderCancelledEvent,
+} from "@joker7nbt-ticketing/common";
+import { Message } from "node-nats-streaming";
+import { QUEUE_GROUP_NAME } from "../queue-group-name";
+import { Ticket } from "../../models/ticket";
+
+export class OrderCancelledListener extends Listener<OrderCancelledEvent> {
+  readonly subject = Subjects.OrderCancelled;
+  queueGroupName = QUEUE_GROUP_NAME;
+
+  async onMessage(data: OrderCancelledEvent["data"], msg: Message) {
+    msg.ack();
+  }
+}
