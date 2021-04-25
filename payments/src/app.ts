e8ab374260f050973ff04c52641ef705768807ec
Toan Nguyen <toan.nguyen@hitatek.io>
2021-04-25 16:04:57 +0700

    add payment module

---
+++ payments/src/app.ts
@@ -0,0 +1,34 @@
+import express from "express";
+import "express-async-errors";
+import cookieSession from "cookie-session";
+import { json } from "body-parser";
+import {
+  errorHandler,
+  NotFoundError,
+  currentUser,
+} from "@joker7nbt-ticketing/common";
+
+const app = express();
+
+app.set("trust proxy", true);
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    //ignore http, just care for https
+    //secure: true
+  })
+);
+app.use(currentUser);
+
+//routers
+
+//handle invalid routes
+app.get("*", async () => {
+  throw new NotFoundError();
+});
+//end routers
+
+app.use(errorHandler);
+
+export { app };
---
+++ payments/src/index.ts
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+import { app } from "./app";
+import { natsWrapper } from "./nats-wrapper";
+
+require("dotenv").config();
+
+const start = async () => {
+  if (!process.env.JWT_SECRET) {
+    throw new Error("JWT is required!");
+  }
+  if (!process.env.MONGO_URI) {
+    throw new Error("Mongo URI is required!");
+  }
+
+  try {
+    await natsWrapper.connect(
+      process.env.NATS_CLUSTER_ID!,
+      process.env.NATS_CLIENT_ID!,
+      process.env.NATS_URL!
+    );
+    natsWrapper.client.on("close", () => {
+      console.log("NATS connection closed!");
+      process.exit();
+    });
+    process.on("SIGINT", () => natsWrapper.client.close());
+    process.on("SIGTERM", () => natsWrapper.client.close());
+
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    });
+    console.log("Connected to mongodb...");
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+start();
+
+app.listen(3000, () => {
+  console.log(`Listening on port 3000!!!`);
+});
