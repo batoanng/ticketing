758670202a15c9547c1a7f8ebd248752783a84c1
Toan Nguyen <toan.nguyen@hitatek.io>
2021-04-27 20:34:34 +0700

    add ticket detail and order detail page

--- client/pages/orders/[orderId].js
+++ client/pages/orders/[orderId].js
@@ -1,5 +1,40 @@
-const TicketShow = () => {
-	return <div>Ticket show</div>;
+import Router from 'next/router';
+import { useEffect, useState } from 'react';
+
+const OrderShow = ({ order }) => {
+	const [timeLeft, setTimeLeft] = useState(0);
+
+	useEffect(() => {
+		const findTimeLeft = () => {
+			const msLeft = new Date(order.expireAt) - new Date();
+			setTimeLeft(Math.round(msLeft / 1000));
+		};
+		findTimeLeft();
+		const timerId = setInterval(findTimeLeft, 1000);
+		if (timeLeft < 0) {
+			clearInterval(timerId);
+		}
+		return () => {
+			clearInterval(timerId);
+		};
+	}, []);
+
+	if (timeLeft < 0) {
+		return <div>Order expired.</div>;
+	}
+	return (
+		<div>
+			<h1>Purchasing Form</h1>
+			Time left to pay: {timeLeft} seconds.
+		</div>
+	);
 };
 
-export default TicketShow;
+OrderShow.getInitialProps = async (context, client, currentUser) => {
+	const { orderId } = context.query;
+	if (!currentUser) Router.push('/');
+	const { data } = await client.get(`/api/orders/${orderId}`);
+	return { order: data };
+};
+
+export default OrderShow;
--- client/pages/tickets/[ticketId].js
+++ client/pages/tickets/[ticketId].js
@@ -1,5 +1,35 @@
-const TicketShow = () => {
-	return <div>Ticket show</div>;
+import Router from 'next/router';
+import useRequest from '../../hooks/use-request';
+
+const TicketShow = ({ ticket }) => {
+	const { doRequest, error } = useRequest(
+		'/api/orders',
+		'post',
+		{
+			ticketId: ticket.id,
+		},
+		(order) => {
+			Router.push(`/orders/${order.id}`);
+		},
+	);
+
+	return (
+		<div>
+			<h1>{ticket.title}</h1>
+			<h4>Price: {ticket.price}</h4>
+			{error}
+			<button onClick={doRequest} className="btn btn-primary">
+				Purchase
+			</button>
+		</div>
+	);
+};
+
+TicketShow.getInitialProps = async (context, client, currentUser) => {
+	const { ticketId } = context.query;
+	if (!currentUser) Router.push('/');
+	const { data } = await client.get(`/api/tickets/${ticketId}`);
+	return { ticket: data };
 };
 
 export default TicketShow;
